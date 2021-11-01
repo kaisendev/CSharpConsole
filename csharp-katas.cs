93b673955ce3f9300825d27844e020ea90947d1a
Blink <tipsyblink11@gmail.com>
2021-10-31 22:29:00 -0700

    update

--- csharp-katas.cs
+++ csharp-katas.cs
@@ -104,5 +104,40 @@ namespace CSharpKatas
                 }
             }
         }
+
+        public static int SquareDigits(int n)
+        {
+            string output = "";
+            foreach (char c in n.ToString())
+            {
+            int square = int.Parse(c.ToString());
+            output += (square * square);
+            }
+            return int.Parse(output);
+        }
+        static int SquareDigits2(int numbers)
+        {
+            int[] number = numbers.ToString().ToCharArray().Select(num => (int)Char.GetNumericValue(num)).ToArray();
+
+            string strNum = string.Empty;
+            int sum = 0;
+
+            foreach (var num in number)
+            {
+                sum = num * num;
+                strNum += sum;
+            }
+            return Int32.Parse(strNum);
+        }
+        public static int SquareDigits3(int n)
+        {
+            StringBuilder sb = new StringBuilder(10);
+            foreach (char ch in n.ToString())
+            {
+                int c = (int)ch - (int)'0';
+                sb.Append(c * c);
+            }
+            return int.Parse(sb.ToString());
+        }
     }
 }
